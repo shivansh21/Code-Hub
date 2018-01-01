4847d087da6f847c0f23cc320734edfb4a1cc59e
Shivansh Sharma <shivanshsharma842@gmail.com>
2018-01-01 13:44:22 +0530

    Create Find_permutations_of_numbered_array.cpp

---
+++ Find_permutations_of_numbered_array.cpp
@@ -0,0 +1,47 @@
+// @ Shivansh Sharma
+
+#include <iostream>
+
+int array[10] = {1,2};
+
+void swap(int x, int y){
+    int temp = array[x];
+    array[x]=array[y];
+    array[y]=temp;
+
+    return;
+}
+
+void printArray(int size){
+    int i;
+
+    for (i=0;i<size;i++)
+        std::cout << array[i] << " ";
+
+    std::cout << std::endl;
+
+    return;
+}
+
+void permute(int k,int size){
+    int i;
+
+    if (k==0)
+        printArray(size);
+    else{
+        for (i=k-1;i>=0;i--){
+            swap(i,k-1);
+            permute(k-1,size);
+            swap(i,k-1);
+        }
+    }
+
+    return;
+}
+
+int main(){
+
+    permute(3,3);
+
+    return 0;
+}
